6d67b2baf16943cb03e5c87098741761ce32bd7c
Mangesh Bodke <mangeshbodke2025@gmail.com>
2025-07-18 01:02:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-document.getElementById("sum_btn").addEventListener("click", function () {
+let a=document.getElementById("sum_btn")
+	a.addEventListener("click", function () {
   const num1 = parseFloat(document.getElementById("n1").value);
   const num2 = parseFloat(document.getElementById("n2").value);
 
-  if (!isNaN(num1) && !isNaN(num2)) {
+ /* if (!isNaN(num1) && !isNaN(num2)) {
     const result = num1 + num2;
     document.getElementById("Sum").textContent = result; // Capital S
   } else {
     document.getElementById("Sum").textContent = "";
   }
+  */
+
+
+		let d= document.getElementById("Sum")
+	let c=num1+num2;
+	
+		d.innerText=c;
+
+	
 });
