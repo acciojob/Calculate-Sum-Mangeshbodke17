01d75bb66fe3ff4a1b8abdfb72d1e799f86b8d47
Mangesh Bodke <mangeshbodke2025@gmail.com>
2025-07-17 23:51:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+
+
+document.getElementById("sum_btn").addEventListener("click", function () {
+  const num1 = parseFloat(document.getElementById("n1").value);
+  const num2 = parseFloat(document.getElementById("n2").value);
+
+  if (!isNaN(num1) && !isNaN(num2)) {
+    const result = num1 + num2;
+    document.getElementById("sum").textContent = "Sum: " + result;
+  } else {
+    document.getElementById("sum").textContent = "";
+  }
+});
